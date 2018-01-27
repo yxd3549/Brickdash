cd59927e614ced756aa54f7417041f77d7a10eeb
JonathanJang <jwj3767@rit.edu>
2018-01-27 15:40:22 -0500

    index page fix

--- index.php
+++ index.php
@@ -3,14 +3,14 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width" initial-scale="1.0" />
-    <title>PHP: Hypertext Preprocessor</title>
+    <title>Brickdash</title>
 </head>
 <body>
     <h1>
-        
+        Welcome to Brickdash!
     </h1>
     <p>
-
+        Come to play! come to win!
     </p>
 </body>
 </html>
