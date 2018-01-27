5c22ad8e7ef3dc84caa5a7882d78bbdb5291acf3
IcarusVi <zscyrus31@gmail.com>
2018-01-27 18:46:03 -0500

    Zach's Push

---
+++ answers.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width" initial-scale="1.0" />
+    <title>Brickdash</title>
+</head>
+<body>
+<div class="container-fluid">
+    <div class="col-md-12">
+        <h1 id='question'> Question? </h1>
+        <form>
+            <input type ="radio" name="answer" value = "Answer"> Answer<br>
+            <input type ="radio" name="answer" value = "Answer"> Answer<br>
+            <input type ="radio" name="answer" value = "Answer"> Answer<br>
+        </form>
+        <button>Submit</button>
+    </div>
+</div>
+</body>
+</html>
