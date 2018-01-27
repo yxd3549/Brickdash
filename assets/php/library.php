cdb1d97329303c02f6767a6c51918afb5bda0fda
yxd3549 <yxd3549@rit.edu>
2018-01-27 15:20:10 -0500

    library functionality

---
+++ assets/php/library.php
@@ -0,0 +1,12 @@
+<?php
+    function generateGroupCode(){
+        $seed = str_split('abcdefghijklmnopqrstuvwxyz'
+            .'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+            .'0123456789!');
+        $rand = "";
+        foreach (array_rand($seed, 6) as $k) $rand .= $seed[$k];
+        echo $rand;
+    }
+    function
+
+?>
