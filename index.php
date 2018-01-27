ea2f4a3228cdf13fb4c22d4ec9bbbb2d0006d3c1
IcarusVi <zscyrus31@gmail.com>
2018-01-27 16:18:24 -0500

    Merge branch 'master' of /Users/zach_cyrus/Desktop/Brickdash with conflicts.

--- index.php
+++ index.php
@@ -1,17 +1,21 @@
 <!DOCTYPE html>
 <html lang="en">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width" initial-scale="1.0" />
-    <title>Brickdash</title>
+    <title>PHP: Hypertext Preprocessor</title>
 </head>
 <body>
-    <h1>
-        Welcome to Brickdash!
-    </h1>
-    <p>
-        Come to play! come to win!
-    </p>
+    <div class="container-fluid text-center" id="welcome">
+        <h1 onclick="window.location = 'www.google.com'">Welcome to Brick Dash</h1>
+        <div >
+            <button type="button" class="btn btn-default" onclick="www.google.com">Join Group</button>
+        </div>
+        <div>
+            <button type="button" class="btn btn-default">Create Group</button>
+        </div>
+    </div>
 </body>
 </html>
-
