65c3e939278200bc923b0c504ebf543c8ab094dc
JonathanJang <jwj3767@rit.edu>
2018-01-27 15:34:27 -0500

    making the winner page

--- finish.php
+++ finish.php
@@ -3,15 +3,17 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width" initial-scale="1.0" />
-    <title>PHP: Hypertext Preprocessor</title>
+    <title>Brickdash</title>
 </head>
 <body>
-    <h1>
-        
-    </h1>
-    <p>
-
-    </p>
+<h1>
+    <center>
+        **Name**<br />
+        Winner!
+    </center>
+</h1>
+<p>
+    honorable mentions
+</p>
 </body>
 </html>
-
