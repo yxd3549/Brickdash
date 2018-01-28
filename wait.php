9bab66461046872028978a213380a6dfc999ba5c
JonathanJang <jwj3767@rit.edu>
2018-01-28 01:23:15 -0500

    ajax stuff

--- wait.php
+++ wait.php
@@ -5,24 +5,36 @@ require_once "assets/php/library.php";
 <html lang="en">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width" initial-scale="1.0" />
-    <title>Brickdash</title>
-</head>
-<body>
-<div class="container-fluid text-center">
-    <div class="col-md-12" id="meh">
-        <span>Access Code: <?= $_SESSION["group"]?></span>
-        <div class="col-md-12" id="playerlist">
-            <?php listPlayers($_SESSION["group"], $mysqli) ?>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width" initial-scale="1.0" />
+        <title>Brickdash</title>
+    </head>
+    <body>
+        <div class="container-fluid text-center">
+            <div class="col-md-12" id="meh">
+                <span>Access Code: <?= $_SESSION["group"]?></span>
+                <div class="col-md-12" id="playerlist">
+                    <script>
+                        function showWaiting() {
+                            var xmlhttp = new XMLHttpRequest();
+                            xmlhttp.onreadystatechange = function() {
+                                if(xmlhttp.readystate==4 && xmlhttp.status==200) {
+                                    document.getElementById("waitingPlayers").innerHTML = this.responseText;
+                                }
+                            };
+                            //xmlhttp.open("GET", "assets/php/library.php?list=" + $_SESSION["group"], true);
+                            xmlhttp.send();
+                            setTimeout(showWaiting, 1000)
+                        }
+                    </script>
+                    <?php listPlayers($_SESSION["group"], $mysqli) ?>
+                </div>
+            </div>
+            <div class="col-md-12">
+                <button> Start Game </button>
+                <button> Leave Game </button>
+            </div>
         </div>
-    </div>
-    <div class="col-md-12">
-        <button onclick="window.location='home.php'"> Start Game </button>
-        <button> Leave Game </button>
-    </div>
-</div>
-</div>
-</body>
+    </body>
 </html>
