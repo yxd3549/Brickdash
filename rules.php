0b7d83b8ed939e61963b94cf0da9f03cdb5ca3b9
JonathanJang <jwj3767@rit.edu>
2018-01-27 19:00:38 -0500

    test the home page

---
+++ rules.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width" initial-scale="1.0" />
+    <title>Brickdash</title>
+</head>
+<body>
+<div class="container-fluid text-center" id="welcome">
+    <h1>Welcome to the Rules Page!</h1>
+    <div >
+        <button type="button" class="btn btn-default" onclick="www.google.com">Join Group</button>
+    </div>
+    <div>
+        <button type="button" class="btn btn-default">Create Group</button>
+    </div>
+</div>
+<p>
+
+</p>
+</body>
+</html>
