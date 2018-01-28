1213670c9a2440ca4db5da4b924f5824df98f2c4
yxd3549 <yxd3549@rit.edu>
2018-01-27 20:41:10 -0500

    Debugging

--- assets/php/library.php
+++ assets/php/library.php
@@ -1,6 +1,16 @@
 <?php
-    include_once "include.php";
-    function generateGroupCode(){
+    error_reporting( E_ALL & ~E_NOTICE);
+    $dbhost = "localhost";
+    $dbuster = "root";
+    $dbpass = "Pupu0405!";
+    $dbname = "brickdash";
+    $mysqli = mysqli_connect($dbhost, $dbuster, $dbpass, $dbname);
+    if ( !$mysqli ){
+        echo "connection failed: " . mysqli_connect_error();
+        die();
+    }
+
+function generateGroupCode(){
         $seed = str_split('abcdefghijklmnopqrstuvwxyz'
             .'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
             .'0123456789!');
