1b1053774dd299a90a811fd0ae1db217ef8c53a8
IcarusVi <zscyrus31@gmail.com>
2018-01-28 04:48:57 -0500

    Css Changes to index font  (Zach)

--- finish.php
+++ finish.php
@@ -2,19 +2,24 @@
 <html lang="en">
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 <link href="https://fonts.googleapis.com/css?family=Space+Mono" rel="stylesheet">
+<link rel="stylesheet" type="text/css" href="assets/css/style.css">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width" initial-scale="1.0" />
     <title>Brickdash</title>
 </head>
-<body>
-    <div class="container-fluid text-center">
-        <h1>Winner you!</h1>
+<body id="finish_body">
+    <div class="container-fluid text-center ">
+        <h1 id="winner">Winner you!</h1>
         <div>
             <p>Thanks for playing!</p>
         </div>
-        <div>
-            <p>Honorable Mentions!</p>
+        <div >
+            <ul id="mentions"> Honorable Mentions!
+                <li> Second</li>
+                <li>Third</li>
+            </ul>
         </div>
     </div>
 </body>
