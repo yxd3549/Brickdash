d935aa8ee0159ee43b9cf1a5ebbf3c99c360ee86
JonathanJang <jwj3767@rit.edu>
2018-01-28 00:23:02 -0500

    formatting

--- wait.php
+++ wait.php
@@ -3,26 +3,25 @@ require_once "assets/php/library.php";
 ?>
 <!DOCTYPE html>
 <html lang="en">
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width" initial-scale="1.0" />
-    <title>Brickdash</title>
-</head>
-<body>
-<div class="container-fluid text-center">
-    <div class="col-md-12" id="meh">
-        <span>Access Code: <?= $_SESSION["group"]?></span>
-        <div class="col-md-12" id="playerlist">
-            <?php listPlayers($_SESSION["group"], $mysqli) ?>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width" initial-scale="1.0" />
+        <title>Brickdash</title>
+    </head>
+    <body>
+        <div class="container-fluid text-  center">
+            <div class="col-md-12" id="meh">
+                <span>Access Code: <?= $_SESSION["group"]?></span>
+                <div class="col-md-12" id="playerlist">
+                    <?php listPlayers($_SESSION["group"], $mysqli) ?>
+                </div>
+            </div>
+            <div class="col-md-12">
+                <button> Start Game </button>
+                <button> Leave Game </button>
+            </div>
         </div>
-    </div>
-    <div class="col-md-12">
-        <button> Start Game </button>
-        <button> Leave Game </button>
-    </div>
-</div>
-</div>
-</body>
+    </body>
 </html>
