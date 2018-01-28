0f73fa8262734ceecab5c5a3d3464fd22d06b8fa
JonathanJang <jwj3767@rit.edu>
2018-01-27 19:54:28 -0500

    Rules page implemented

--- rules.php
+++ rules.php
@@ -8,17 +8,42 @@
     <title>Brickdash</title>
 </head>
 <body>
-<div class="container-fluid text-center" id="welcome">
+<div class="container-fluid text-center" id="rules">
     <h1>Welcome to the Rules Page!</h1>
-    <div >
-        <button type="button" class="btn btn-default" onclick="www.google.com">Join Group</button>
-    </div>
-    <div>
-        <button type="button" class="btn btn-default">Create Group</button>
-    </div>
 </div>
-<p>
-
-</p>
+<div class="container-fluid text-right" id="rulesbtn">
+    <button type="button" class="btn btn-default" onclick="index.php">Back!</button>
+</div>
+<div class="container-fluid text-center" id="rules">
+    <h3>Basics</h3>
+    <p>
+        First and foremost, it is essential for all players to turn on their thinking caps! After completing this step,
+        make sure that all of your friends are in the list. Finally, enjoy the game!
+    </p>
+    <h3>The Game</h3>
+    <p>
+        The goal is to get the most possible points within the number of rounds that is specified by you the players.
+        After all players are in your waiting room, we can then start off with the first round! First, players will
+        enter their answers to the question. Once all players have submitted an answer ***within the given time frame,
+        *** all players will vote on what they believe is the correct answer or which is the most appealing to them.
+        Lastly, make memorable stories, memes and always have fun!
+    </p>
+    <h3>Scoring Points</h3>
+    <p>
+        Players will get points based off of how other players chose answered questions. The different possible ways
+        to recieve points are listed below:
+    </p>
+    <ul>
+        <li>
+            If your answer recieves votes, then for every vote recieved players will SCORE ONE point.
+        </li>
+        <li>
+            If you vote for the real answer, then you will SCORE TWO points.
+        </li>
+    </ul>
+</div>
+<div class="container-fluid text-right" id="rulesbtn">
+    <button type="button" class="btn btn-default" onclick="index.php">Back!</button>
+</div>
 </body>
 </html>
