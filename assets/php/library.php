126ab00acd589e0ba3cd9cf9462abe6b8b825604
yxd3549 <yxd3549@rit.edu>
2018-05-07 14:18:54 -0400

    Update library.php

--- assets/php/library.php
+++ assets/php/library.php
@@ -9,7 +9,7 @@
     error_reporting( E_ALL & ~E_NOTICE);
     $dbhost = "localhost";
     $dbuster = "root";
-    $dbpass = "Pupu0405!";
+    $dbpass = "";
     $dbname = "brickdash";
     $mysqli = mysqli_connect($dbhost, $dbuster, $dbpass, $dbname);
     if ( !$mysqli ){
